b92032a19fdc309361ae31389baeb0e1c39d26dd
Stewart MacKenzie-Leigh <stewart@stugo.co.uk>
2015-04-25 19:21:42 +0100

    Fix image load detection

--- index.js
+++ index.js
@@ -1,66 +1,68 @@
 
-imagesLoaded('.reel', function () {
-  var imgs = [];
-  var index = 0;
-  var timeout = null;
-  var small = false;
-  
-  resize();
-  
-  function next(inc) {
-    var img = imgs[index = (index + inc) % imgs.length];
-    $('.reel').animate({left: -img.offset}, 500, function () {
-      img.caption.offset({left: img.caption.parent().offset().left + 40});
-      $('.caption').css('opacity', '0');
-      img.caption.css('opacity', '1');
-    });
-  };
-  
-  $(window).resize(function () {
-    if (timeout) {
-      window.clearTimeout(timeout);
-    }
-    timeout = window.setTimeout(resize, 200);
-  });
-  
-  function resize() {
-    var height = $('#showcase').height();
-    var width = $('#showcase').width();
-    var reelwidth = 0;
-    imgs = [];
-    small = $(this).width() <= 1200;
+$(function () {
+  imagesLoaded('#showcase', function () {
+    var imgs = [];
+    var index = 0;
+    var timeout = null;
+    var small = false;
 
-    $('#showcase .img img').each(function () {
-      var w = $(this).height(height).width() + 10;
-      imgs.push({offset: reelwidth - (width - w) / 2, caption: $('.caption', $(this).parent())});
-      reelwidth += w;
-    });
+    resize();
 
-    $('.reel').width(reelwidth);
-    next(0);
-  }
-  
-  $('.left-arrow').click(next.bind(null, -1));
-  $('.right-arrow').click(next.bind(null, 1));
-  
-  $('nav a').click(function () {
-    if (!small) {
-      var link = this.href.match(/#(.*)$/) || [];
-      
-      if (link[1]) {
-        if (link[1] === 'showcase') {
-          $('html,body').animate({scrollLeft: 0});
-        } else {
-          $('html,body').animate({scrollLeft: $('nav').offset().left + 'px'});
-          
-          var bookmark = $('a[name="' + link[1] + '"]');
-          var top = $('#copy').scrollTop() + bookmark.offset().top;
-          $('#copy').animate({scrollTop: top + 'px'});
-          bookmark.parent().animate({scrollTop: 0});
-        }
+    function next(inc) {
+      var img = imgs[index = (index + inc) % imgs.length];
+      $('.reel').animate({left: -img.offset}, 500, function () {
+        img.caption.offset({left: img.caption.parent().offset().left + 40});
+        $('.caption').css('opacity', '0');
+        img.caption.css('opacity', '1');
+      });
+    };
+
+    $(window).resize(function () {
+      if (timeout) {
+        window.clearTimeout(timeout);
       }
-      
-      return false;
+      timeout = window.setTimeout(resize, 200);
+    });
+
+    function resize() {
+      var height = $('#showcase').height();
+      var width = $('#showcase').width();
+      var reelwidth = 0;
+      imgs = [];
+      small = $(this).width() <= 1200;
+
+      $('#showcase .img img').each(function () {
+        var w = $(this).height(height).width() + 10;
+        imgs.push({offset: reelwidth - (width - w) / 2, caption: $('.caption', $(this).parent())});
+        reelwidth += w;
+      });
+
+      $('.reel').width(reelwidth);
+      next(0);
     }
+
+    $('.left-arrow').click(next.bind(null, -1));
+    $('.right-arrow').click(next.bind(null, 1));
+
+    $('nav a').click(function () {
+      if (!small) {
+        var link = this.href.match(/#(.*)$/) || [];
+
+        if (link[1]) {
+          if (link[1] === 'showcase') {
+            $('html,body').animate({scrollLeft: 0});
+          } else {
+            $('html,body').animate({scrollLeft: $('nav').offset().left + 'px'});
+
+            var bookmark = $('a[name="' + link[1] + '"]');
+            var top = $('#copy').scrollTop() + bookmark.offset().top;
+            $('#copy').animate({scrollTop: top + 'px'});
+            bookmark.parent().animate({scrollTop: 0});
+          }
+        }
+
+        return false;
+      }
+    });
   });
 });
